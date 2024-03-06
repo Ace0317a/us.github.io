ba30cf55f8a8d916866396b6269ec0b8c40ad696
Ace0317a <pravalchaudhary0000@gmail.com>
2024-03-06 16:14:29 +0530

    us

--- script.js
+++ script.js
@@ -72,7 +72,46 @@ class Paper {
       this.rotating = false;
     });
   }
+
+  init(paper) {
+    paper.addEventListener('touchmove', (e) => {
+      e.preventDefault();
+      this.touchMoveX = e.touches[0].clientX;
+      this.touchMoveY = e.touches[0].clientY;
+
+      this.velX = this.touchMoveX - this.prevTouchX;
+      this.velY = this.touchMoveY - this.prevTouchY;
+
+      if (this.holdingPaper) {
+        this.currentPaperX += this.velX;
+        this.currentPaperY += this.velY;
+
+        paper.style.transform = `translateX(${this.currentPaperX}px) translateY(${this.currentPaperY}px)`;
+      }
+      
+      this.prevTouchX = this.touchMoveX;
+      this.prevTouchY = this.touchMoveY;
+    });
+
+    paper.addEventListener('touchstart', (e) => {
+      if (this.holdingPaper) return;
+      this.holdingPaper = true;
+
+      paper.style.zIndex = highestZ;
+      highestZ += 1;
+
+      this.touchStartX = e.touches[0].clientX;
+      this.touchStartY = e.touches[0].clientY;
+      this.prevTouchX = this.touchStartX;
+      this.prevTouchY = this.touchStartY;
+    });
+
+    paper.addEventListener('touchend', () => {
+      this.holdingPaper = false;
+    });
+  }
 }
+
 
 const papers = Array.from(document.querySelectorAll('.paper'));
 
